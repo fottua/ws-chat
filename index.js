d6a1a75bdbcbabfabc88d12f2e63a86ab41d03ef
fottua <fott.ua@gmail.com>
2015-03-22 20:04:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
+var WebSocketServer = new require('ws').Server;
+var http = require('http');
 var express = require("express")
 var app = express()
 var port = process.env.PORT || 5000
+
+
 
 app.use(express.static(__dirname + "/"))
 
 var server = http.createServer(app)
 server.listen(port)
-
 console.log("http server listening on %d", port)
 
+// подключенные клиенты
+var clients = {};
 var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
+// WebSocket-сервер на порту 8081
+wss.on('connection', function(ws) {
+  var animal = ["Капитошка", "Звероящер", "АгрессивныйХомяк", "ПлотоядныйБык", "СобакоЗавр", "Кот", "Паук", "Иссушитель", "Троль1000лвл"];
+  var id = animal[Math.round(Math.random()*6)];
+  clients[id] = ws;
+  console.log("новое соединение " + id);
 
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+  ws.on('message', function(message) {
+    console.log('получено сообщение ' + message);
 
-  console.log("websocket connection open")
+    for(var key in clients) {
+      clients[key].send(id + ': ' + message);
+    }
+  });
 
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
+  ws.on('close', function() {
+    console.log('соединение закрыто ' + id);
+    delete clients[id];
+  });
+
+});
