8400e0fd5e1fbbcc7895104a39fff0c48c289efc
fottua <fott.ua@gmail.com>
2015-03-22 22:19:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var WebSocketServer = new require('ws').Server;
 var http = require('http');
 var express = require("express")
 var app = express()
-var port = process.env.PORT || 5000
+var port = process.env.PORT || 80
 
 
 
