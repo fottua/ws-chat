74f1a31b0bc5c8b1293e0ae535e5b1d01383ac11
fottua <fott.ua@gmail.com>
2016-01-18 22:11:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ wss.on('connection', function(ws) {
     
     for(var key in clients) {
       clients[key].send(id + ':@-Вышел из чата');
-      delete clients[key];
+      clients[key].send(clients[key]);
     }
     
   });
